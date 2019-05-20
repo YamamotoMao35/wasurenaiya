99e05c1a834ed24767525a385b87850f5ecc29c1
YamamotoMao35 <nokiroram@gmail.com>
2019-05-20 10:41:25 +0900

    Update HelloController.php
    Update HelloController.php
    
    HelloControllerの記述を変更し、ルーティングを変更してページを表示することができた。

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+public function index() {
+return <<<EOF
+<html>
+<head>
+<title>Hello/index</title>
+<style>
+    body {
+        font-size:16pt; 
+        color:#999;
+    }
+    h1 {
+        font-size:100pt;
+        text-align: right;
+        color:#eee;
+        margin:-40px 0 -50px 0;
+    }
+    </style>
+</head>
+<body>
+<h1>index</h1>
+<p>こちらはHelloコントローラーのindexページです</p>
+</body>
+</html>
+EOF;
+}
 }
