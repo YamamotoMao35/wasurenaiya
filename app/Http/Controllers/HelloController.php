bc20a868f2718a7a971023ef7f48b92f91e7271d
YamamotoMao35 <nokiroram@gmail.com>
2019-05-20 19:46:24 +0900

    Update HelloRequest.php
    Update HelloRequest.php
    
    ファイル内に検証ルールを作成し、リクエストの中で検証ができるようにした。

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,20 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends controller{
     public function index(Request $request){
         return view('hello.index',['msg'=>'フォームを入力:']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
         return view('hello.index',['msg'=>'正しく入力されました!']);
     }
 }
