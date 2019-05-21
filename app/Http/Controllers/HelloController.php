c29839532b3aab135b4fdbf5e2a31a01d3a95692
YamamotoMao35 <nokiroram@gmail.com>
2019-05-21 16:15:47 +0900

    Update HelloController.php
    Update HelloController.php
    
    クエリビルダによるCRUDの基本を学んだ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends controller{
     public function index(Request $request){
-        $items = DB::table('people')->get();
+        $items = DB::table('people')->orderBy('age','asc')->get();
         return view('hello.index',['items'=>$items]);
     }
 
@@ -31,15 +31,14 @@ class HelloController extends controller{
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::insert('insert into people (name,mail,age) values(:name,:mail,:age)',$param);
+        DB::table('people')->insert($param);
         return redirect('/hello');
     }
 
     public function edit(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
     }
 
     public function update(Request $request)
@@ -50,28 +49,28 @@ class HelloController extends controller{
             'mail' => $request->mail,
             'age' =>$request->age,
         ];
-        DB::update('update people set name = :name,mail= :mail,age= :age where id = :id', $param);
+        DB::table('people')->where('id', $request->id)->update($param);
         return redirect('/hello');
     }
 
     public function del(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.del', ['form' => $item[0]]);
+        $item = DB::table('people')->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
     }
 
     public function remove(Request $request)
     {
-        $param = ['id' => $request->id];
-        DB::delete('delete from people where id = :id', $param);
+        DB::table('people')->where('id', $request->id)->delete();
         return redirect('/hello');
     }
 
     public function show(Request $request)
     {
-        $id = $request->id;
-        $item = DB::table('people')->where('id', '<=', $id)->get();
-        return view('hello.show', ['items' => $item]);
+        $min = $request->min;
+        $max = $request->max;
+        $items = DB::table('people')
+            ->whereRaw('age >= ? and age <= ?', [$min, $max])->get();
+        return view('hello.show',['items' => $items]);
     }
 }
