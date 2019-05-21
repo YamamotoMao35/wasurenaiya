1157ad12c4a9052995dd9abaf3449d0f9fe2476e
YamamotoMao35 <nokiroram@gmail.com>
2019-05-21 14:05:41 +0900

    Update HelloController.php
    Update HelloController.php
    
    $paramsに送信フォームの値を保管して、あとはこの配列をパラメータ引数にしてDB::insertを呼び出し実行するだけ。
    valuesでパラメータ配列$paramsから3つの値を嵌め込むようにしている。
    これで送信されたフォームの値を元にinsert文を実行する、という処理ができた。

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,18 +9,29 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends controller{
     public function index(Request $request){
-        if (isset($request->id))
-        {
-            $param = ['id'=>$request->id];
-            $items = DB::select('select * from people where id = :id', $param);
-        } else {
-            $items = DB::select('select * from people');
-        }
+        $items = DB::select('select * from people');
         return view('hello.index',['items'=>$items]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        return view('hello.index',['msg'=>'正しく入力されました!']);
+        $items = DB::select('select * from people');
+        return view('hello.index',['items'=>$items]);
+    }
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name,mail,age) values(:name,:mail,:age',$param);
+        return redirect('/hello');
     }
 }
