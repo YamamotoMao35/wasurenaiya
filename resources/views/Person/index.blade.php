1d7b656ce5aad2a611249c9187e6d71a72ce311f
YamamotoMao35 <nokiroram@gmail.com>
2019-05-22 10:34:14 +0900

    Update index.blade.php
    Update index.blade.php
    
    コントローラとビューを修正し、値を持っている人と持っていない人に分けた

--- resources/views/Person/index.blade.php
+++ resources/views/Person/index.blade.php
@@ -10,19 +10,28 @@
 @section('content')
   <table>
     <tr><th>Person</th><th>Board</th></tr>
-    @foreach($items as $item)
+    @foreach($hasItems as $item)
     <tr>
       <td>{{$item->getData()}}</td>
-      <td>@if ($item->board != null)
+      <td>
         <table width="100%">
           @foreach ($item->boards as $obj)
           <tr><td>{{$obj->getData()}}</td></tr>
           @endforeach
         </table>
-        @endif
       </td>
     </tr>
     @endforeach
+  </table>
+  <div style="margin:10px;"></div>
+    <table>
+      <tr><th>Person</th></tr>
+      @foreach ($noItems as $item)
+      <tr>
+        <td>{{$item->getData()}}</td>
+      </tr>
+      @endforeach
+    </table>
   </table>
 @endsection
 
